abe96b5bc1d612114b04d9fb7de7a670e2c3e997
Jesse Zomer <jessezomer@hotmail.com>
2020-07-30 16:03:52 +0200

    added failing testcase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,43 @@
-import { TestBed, async } from '@angular/core/testing';
+import { CdkDragPreview } from '@angular/cdk/drag-drop';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockDirective, ngMocks, MockedDirective } from 'ng-mocks';
+
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MockDirective(CdkDragPreview)
       ],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngMocksClassDirective'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngMocksClassDirective');
+  it('should add the extra css class to the preview', () => {
+    const preview = ngMocks.findInstance(fixture.debugElement, CdkDragPreview) as MockedDirective<CdkDragPreview>;
+    preview.__render();
+
+    // don't know how to get the classes from the preview, so do a normal query
+    expect(fixture.debugElement.query(By.css('.dragdrop-preview')).nativeElement).toHaveClass('extra-class');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngMocksClassDirective app is running!');
-  });
+
+
 });
